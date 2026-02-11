4b4a5dcc8d4c26d2d6be36b8f10e7ff37b0f43dd
nguyenan12 <nguyenann1204@gmail.com>
2026-02-11 15:03:45 +0700

    refactor structure project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import App from '~/App.jsx'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CssVarsProvider >
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>
 )
